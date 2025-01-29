ffe8725fe09b16e8dec52fe3c4e7215837b107b9
fatima <73296091+fatimabaiji@users.noreply.github.com>
2025-01-29 11:12:35 +0000

    Refactor answer feedback alerts and implement score tracking functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 
 // get the button elemts and add event listeners to them 
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons){
@@ -48,9 +48,11 @@ function checkAnswer(){
     let isCorrect = userAnswer === calculatedAnswer[0];
 
     if(isCorrect){
-        alert("Hey! You got it right! :D");
+        alert('Hey! You got it right! :D');
+        incrementScore();
     } else {
-        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        alert(`Awwww....you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -75,11 +77,21 @@ function calculateCorrectAnswer(){
     throw `Unimplemented operator ${operator}. Aborting!`;
 }
 }
-function incrementScore(){
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+function incrementScore(){
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer(){
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 
 }
 
